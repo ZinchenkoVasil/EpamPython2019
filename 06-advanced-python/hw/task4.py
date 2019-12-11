f8275b6a2bde315b2c80df7df99ec64115510ba8
vasil <zinchenkovasil@mail.ru>
2019-12-11 10:09:38 +0300

    tree

--- 06-advanced-python/hw/task4.py
+++ 06-advanced-python/hw/task4.py
@@ -1,3 +1,70 @@
 """
-Написать тесты(pytest) к предыдущим 3 заданиям, запустив которые, я бы смог бы проверить их корректность
+Реализовать метод __str__, позволяющий выводить все папки и файлы из данной, например так:
+
+> print(folder1)
+
+V folder1
+|-> V folder2
+|   |-> V folder3
+|   |   |-> file3
+|   |-> file2
+|-> file1
+
+А так же возможность проверить, находится ли файл или папка в другой папке:
+> print(file3 in folder2)
+True
+
 """
+import os
+
+class PrintableFolder:
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+    def __str__(self):
+        if os.path.exists(self.name):
+            final_str = self.name + '\n'
+            return self._tree(self.name, -1, final_str)
+        else:
+            print('File not found!')
+
+    def _tree(self, dir_name, level, final_str):
+        level += 1
+        for file_name in os.listdir(dir_name):
+            final_str += level * ' ' + '|->' + file_name + '\n'
+            full_file_name = os.path.join(dir_name, file_name)
+
+            if os.path.isdir(full_file_name):
+                final_str += self._tree(full_file_name, level, final_str)
+        return final_str
+
+
+class PrintableFile:
+    def __init__(self, name, dir_name):
+        self.name = name
+        self.dir_name = dir_name
+
+    def __str__(self):
+        return str(self._search(self.dir_name, self.name))
+
+    def _search(self, dir_name, searched_name):
+        full_file_name = os.path.join(dir_name, searched_name)
+        if os.path.exists(full_file_name):
+            return True
+        else:
+            for file_name in os.listdir(dir_name):
+                full_file_name = os.path.join(dir_name, file_name)
+                if os.path.isdir(full_file_name):
+                    if self._search(full_file_name, searched_name):
+                        return True
+        return False
+
+printableFolder = PrintableFolder(r'D:\EpamPython2019\06-advanced-python','111')
+print(printableFolder)
+
+printableFile = PrintableFile('task2.py', r'D:\EpamPython2019\06-advanced-python')
+print(printableFile)
+
+
+
