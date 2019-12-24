21eb19d44e9b87877b31e494427c175275c57e91
vasil <zinchenkovasil@mail.ru>
2019-12-24 23:47:36 +0300

    NEW VERSION

--- 10-Design_Patterns/hw/hw.py
+++ 10-Design_Patterns/hw/hw.py
@@ -1,28 +1,29 @@
+DUR_FROM_FACTORY_TO_PORT = 1
+DUR_FROM_FACTORY_TO_STORAGE_B = 5
+DUR_FROM_PORT_TO_STORAGE_A = 4
 
 class Factory:
      products = {'A': 0, 'B': 0}
-     a = products['A'] #current count of product A
-     b = products['B'] #current count of product B
+     curr_products = {'A': 0, 'B': 0} #current count of product A
+#     b = products['B'] #current count of product B
 
      @classmethod
      def create_product(cls, line_products):
          cls.products = {'A': 0, 'B': 0}
+         cls.curr_products = {'A': 0, 'B': 0}
+
          for litera in line_products:
              if litera in cls.products:
                  cls.products[litera] += 1
+                 cls.curr_products[litera] += 1
              else:
                  cls.products[litera] = 1
-         cls.a = cls.products['A']
-         cls.b = cls.products['B']
+                 cls.curr_products[litera] = 1
 
 class Port:
-    a = 0  #current count of product A
+    curr_products = {'A': 0}  #current count of product A
 
-class StorageA:
-    a = 0  #current count of product A
-
-class StorageB:
-    b = 0  #current count of product B
+storage = {'A': 0, 'B': 0}
 
 class Transport:
     def __init__(self):
@@ -32,32 +33,31 @@ class Transport:
         if self.duration > 0:
             self.duration -= 1
 
-
 class Truck(Transport):
-    def moveA_from_factory_to_port(self):
-        if (Factory.a > 0) and (self.duration == 0):
-            Factory.a -= 1
-            self.duration = 2 * 1
+    def move_from_factory_to_port(self, product):
+        if (Factory.curr_products[product] > 0) and (self.duration == 0):
+            Factory.curr_products[product] -= 1
+            self.duration = DUR_FROM_FACTORY_TO_PORT * 2
 
-        if (self.duration == 1):
-            Port.a += 1
+        if (self.duration == DUR_FROM_FACTORY_TO_PORT):
+            Port.curr_products[product] += 1
 
-    def moveB_from_factory_to_storageB(self):
-        if (Factory.b > 0) and (self.duration == 0):
-            Factory.b -= 1
-            self.duration = 2 * 5
+    def move_from_factory_to_storage(self, product):
+        if (Factory.curr_products[product] > 0) and (self.duration == 0):
+            Factory.curr_products[product] -= 1
+            self.duration = 2 * DUR_FROM_FACTORY_TO_STORAGE_B
 
-        if self.duration == 5:
-            StorageB.b += 1
+        if self.duration == DUR_FROM_FACTORY_TO_STORAGE_B:
+            storage[product] += 1
 
 class Ship(Transport):
-    def moveA_from_port_to_storageA(self):
-        if (Port.a > 0) and (self.duration == 0):
-            Port.a -= 1
-            self.duration = 2 * 4
+    def move_from_port_to_storage(self, product):
+        if (Port.curr_products[product] > 0) and (self.duration == 0):
+            Port.curr_products[product] -= 1
+            self.duration = 2 * DUR_FROM_PORT_TO_STORAGE_A
 
-        if self.duration == 4:
-            StorageA.a += 1
+        if self.duration == DUR_FROM_PORT_TO_STORAGE_A:
+            storage[product] += 1
 
 def main():
     line_products = input("Input list of contaners (for example 'AAB'): ")
@@ -68,18 +68,18 @@ def main():
     ship1 = Ship()
     timer = 0
 
-    print("Count of product A: ",Factory.a)
-    print("Count of product B: ",Factory.b)
+    print("Count of product A: ",Factory.curr_products['A'])
+    print("Count of product B: ",Factory.curr_products['B'])
 
-    while (StorageA.a < Factory.products['A']) or (StorageB.b < Factory.products['B']):
+    while (storage['A'] < Factory.products['A']) or (storage['B'] < Factory.products['B']):
         truck1.step_timer()
         truck2.step_timer()
         ship1.step_timer()
-        truck1.moveA_from_factory_to_port()
-        truck1.moveB_from_factory_to_storageB()
-        truck2.moveB_from_factory_to_storageB()
-        truck2.moveA_from_factory_to_port()
-        ship1.moveA_from_port_to_storageA()
+        truck1.move_from_factory_to_port('A')
+        truck1.move_from_factory_to_storage('B')
+        truck2.move_from_factory_to_storage('B')
+        truck2.move_from_factory_to_port('A')
+        ship1.move_from_port_to_storage('A')
         timer += 1
 
     print("delivery time: ", timer - 1)
