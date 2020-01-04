acbd4f69098bf9f100f66129192c9a5e08679c92
vasil <zinchenkovasil@mail.ru>
2020-01-04 19:58:29 +0300

    upgrade

--- 11-Data-Persistence/DBase.py
+++ 11-Data-Persistence/DBase.py
@@ -10,8 +10,9 @@ import copy
 import psycopg2
 #Базовый класс и есть методо Out_to_db()
 class Connection:
-    def __init__(self, db_name):
+    def __init__(self, db_name, conn_string):
         self.db_name = db_name
+        self.conn_string = conn_string
 
     def select_db(self, collection_name):
         raise Exception('This method without realisation!')
@@ -20,10 +21,13 @@ class Connection:
         raise Exception('This method without realisation!')
 
 class Connection_json(Connection):
+    def __init__(self, db_name, conn_string):
+        super().__init__(db_name, conn_string)
+        self.filename = os.path.join(self.conn_string, self.db_name)
+
     def out_to_db(self, out_list, collection_name):
         try:
-            print(self.db_name)
-            with open(self.db_name, 'w', encoding='utf-8') as f:
+            with open(self.filename, 'w', encoding='utf-8') as f:
                 json.dump(out_list, f, ensure_ascii=False)
                 print(f"файл {self.db_name} создался успешно!")
                 return True
@@ -32,9 +36,9 @@ class Connection_json(Connection):
              return False
 
     def select_db(self, collection_name):
-        if os.path.exists(self.db_name):
+        if os.path.exists(self.filename):
             # Читаем JSON из файла и преобразуем к типу Python
-            with open(self.db_name, 'r', encoding='UTF-8') as f:
+            with open(self.filename, 'r', encoding='UTF-8') as f:
                 read_data = json.load(f)
                 pprint.pprint(read_data)
         else:
@@ -43,8 +47,8 @@ class Connection_json(Connection):
 
 class Connection_mongoDB(Connection):
     def __init__(self, db_name, conn_string):
-        super().__init__(db_name)
-        client = MongoClient(conn_string)#'mongodb://127.0.0.1:27017')
+        super().__init__(db_name, conn_string)
+        client = MongoClient(self.conn_string)#'mongodb://127.0.0.1:27017')
         self.db = client[self.db_name]
 
     def out_to_db(self, out_list, collection_name):
@@ -58,20 +62,20 @@ class Connection_mongoDB(Connection):
             print(obj)
 
 class Connection_postgreSQL(Connection):
-    def __init__(self, db_name, user, password, host, port):
-        super().__init__(db_name)
-        self.user = user
-        self.password = password
-        self.host = host
-        self.port = port
+    def __init__(self, db_name, conn_string):
+        super().__init__(db_name, conn_string)
+#        self.user = user
+#        self.password = password
+#        self.host = host
+#        self.port = port
 
-        self.conn = psycopg2.connect(
-            database = self.db_name,
-            user = self.user,#"postgres",
-            password = self.password, # "111111",
-            host = self.host, # "127.0.0.1",
-            port = self.port #"5432"
-        )
+        self.conn = psycopg2.connect(self.conn_string)
+#            database = self.db_name,
+#            user = self.user,#"postgres",
+#            password = self.password, # "111111",
+#            host = self.host, # "127.0.0.1",
+#            port = self.port #"5432"
+#        )
 
     def create_table(self, table_name, out_dict): #conn
 # Схема определяет таблицы в базе данных
@@ -121,7 +125,10 @@ out_lst_copy1 = copy.deepcopy(out_lst)
 out_lst_copy2 = copy.deepcopy(out_lst)
 
 print("JSON")
-conn = Connection_json('employees.json')
+conn = Connection_json('employees.json',r'D:\EPAMPython2019_NEW_2\11-Data-Persistence') #Windows
+
+#conn = Connection_json('employees.json',r'file:///home/user/data') #Linux
+
 conn.out_to_db(out_lst,'')
 conn.select_db('employees')
 
@@ -131,7 +138,7 @@ conn.out_to_db(out_lst_copy1,'employees')
 conn.select_db('employees')
 
 print("postgreSQL")
-conn = Connection_postgreSQL('postgres', 'postgres', '111111', "127.0.0.1", "5432")
+conn = Connection_postgreSQL('postgres', 'postgresql://postgres:111111@127.0.0.1:5432/postgres')
 conn.create_table('employees', out_lst_copy2[0])
 conn.out_to_db(out_lst_copy2, 'employees')
 conn.select_db('employees')
