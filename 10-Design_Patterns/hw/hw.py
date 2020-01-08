865e0d03a1f712398d0befea5778d9e21d6c37d3
vasil <zinchenkovasil@mail.ru>
2020-01-08 11:30:00 +0300

    add class Storage

--- 10-Design_Patterns/hw/hw.py
+++ 10-Design_Patterns/hw/hw.py
@@ -30,7 +30,9 @@ class Factory(Hub):
 class Port(Hub):
     pass
 
-storages = {'A': 0, 'B': 0}
+class Storage:
+    def __init__(self):
+        self.products = {'A': 0, 'B': 0}
 
 class Transport:
     def __init__(self):
@@ -42,20 +44,20 @@ class Transport:
             self.duration -= 1
 
 class Truck(Transport):
-    def move_product(self, factory, port, lst_product_names):
+    def move_product(self, factory, port, storage, lst_product_names):
         self._step_timer()
         if self.duration == 0:
             self.curr_product = factory.get_product_from_queue()
-            if self.curr_product == lst_product_names[0]:
+            if (len(lst_product_names) > 0) and (self.curr_product == lst_product_names[0]):
                 self._from_factory_to_port()
-            elif self.curr_product == lst_product_names[1]:
+            elif (len(lst_product_names) > 1) and (self.curr_product == lst_product_names[1]):
                 self._from_factory_to_storage()
 
-        if (self.duration == DUR_FROM_FACTORY_TO_PORT) and (self.curr_product == lst_product_names[0]):
+        if (self.duration == DUR_FROM_FACTORY_TO_PORT) and (len(lst_product_names) > 0) and (self.curr_product == lst_product_names[0]):
             port.put_product_to_queue(self.curr_product)
 
-        if (self.duration == DUR_FROM_FACTORY_TO_STORAGE_B) and (self.curr_product == lst_product_names[1]):
-            storages[self.curr_product] += 1
+        if (self.duration == DUR_FROM_FACTORY_TO_STORAGE_B) and (len(lst_product_names) > 1) and (self.curr_product == lst_product_names[1]):
+            storage.products[self.curr_product] += 1
 
     def _from_factory_to_port(self):
         self.duration = DUR_FROM_FACTORY_TO_PORT * 2
@@ -64,15 +66,15 @@ class Truck(Transport):
         self.duration = DUR_FROM_FACTORY_TO_STORAGE_B * 2
 
 class Ship(Transport):
-    def move_from_port_to_storage(self, port, lst_product_names):
+    def move_from_port_to_storage(self, port, storage, lst_product_names):
         self._step_timer()
         if self.duration == 0:
             self.curr_product = port.get_product_from_queue()
             if self.curr_product == lst_product_names[0]:
                 self.duration = 2 * DUR_FROM_PORT_TO_STORAGE_A
 
-        if (self.duration == DUR_FROM_PORT_TO_STORAGE_A) and (self.curr_product == lst_product_names[0]):
-            storages[self.curr_product] += 1
+        if (self.duration == DUR_FROM_PORT_TO_STORAGE_A) and (len(lst_product_names) > 0) and (self.curr_product == lst_product_names[0]):
+            storage.products[self.curr_product] += 1
 
 def main():
     line_products = input("Input list of contaners (for example 'AAB'): ")
@@ -82,16 +84,18 @@ def main():
     truck1 = Truck()
     truck2 = Truck()
     ship1 = Ship()
+    storageA = Storage()
+    storageB = Storage()
 
     print("Count of product A: ",factory1.products['A'])
     print("Count of product B: ",factory1.products['B'])
     timer = 0
-    while (storages['A'] < factory1.products['A']) or (storages['B'] < factory1.products['B']):
-        #print(storageA.product)
-        #print(storageB.product)
-        truck1.move_product(factory1, port1, ['A','B'])
-        truck2.move_product(factory1, port1, ['A','B'])
-        ship1.move_from_port_to_storage(port1, ['A'])
+    while (storageA.products['A'] < factory1.products['A']) or (storageB.products['B'] < factory1.products['B']):
+#        print(storageA.products['A'])
+#        print(storageB.products['B'])
+        truck1.move_product(factory1, port1, storageB, ['A','B'])
+        truck2.move_product(factory1, port1, storageB, ['A','B'])
+        ship1.move_from_port_to_storage(port1, storageA, ['A'])
         timer += 1
 
     print("delivery time: ", timer - 1)
